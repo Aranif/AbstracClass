1fcbddf1cad0997313adc61c14c17e9d5fa1fa24
Aranif <165161315+Aranif@users.noreply.github.com>
2024-04-04 22:49:23 +0200

    Update Main.java

--- src/fp2/poo/practica7/utilidades/Main.java
+++ src/fp2/poo/practica7/utilidades/Main.java
@@ -1,23 +1,25 @@
 package fp2.poo.practica7.utilidades;
 import fp2.poo.practica7.utilidades.*;
 import java.lang.*; 
+//Install this class in your PC
 import java.awt.Color;
 
 
 public class Main{
 	public static void main(String args[]){
 	
-	//Los dos objetos reciven un obj de tipo color
+	//Two objets( of type Circulo and Cuadrado ) receive 
+	//objets of type Color
 	Color color = new Color((int)21.3, (int)24.3, (int)43.1);
-    //Declaro e instancio objetos
+    // I declare and intantiate the objects c and s
     Circulo c = new Circulo((float)15.2, color);
     Cuadrado s = new Cuadrado((float)2.3, color);
     
-    //Imprime areas
+    //Print areas
     System.out.println("Area del circulo: "+c.getArea());
     System.out.println("Area del cuadrado: "+s.getArea());
     
-    //uso de "toString para imprimir el color
+    // "toString use to print the color 
    System.out.println(c);
    System.out.println(s);
     
