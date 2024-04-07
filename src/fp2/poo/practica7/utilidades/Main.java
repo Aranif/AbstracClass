1f53902b2e9f501663f80d4f848c1b91c073ea25
Aranif <165161315+Aranif@users.noreply.github.com>
2024-04-07 12:03:03 +0200

    Update Main.java

--- src/fp2/poo/practica7/utilidades/Main.java
+++ src/fp2/poo/practica7/utilidades/Main.java
@@ -1,7 +1,7 @@
 package fp2.poo.practica7.utilidades;
 import fp2.poo.practica7.utilidades.*;
 import java.lang.*; 
-//Install this class in your PC
+//Install this class in your PC.
 import java.awt.Color;
 /**
 * @(#)Figura.java
@@ -15,18 +15,20 @@ import java.awt.Color;
 public class Main{
 	public static void main(String args[]){
 	
-	//Two objets( of type Circulo and Cuadrado ) receive 
+	//Two objets( of type Circulo and Cuadrado ) receive.
 	//objets of type Color
 	Color color = new Color((int)21.3, (int)24.3, (int)43.1);
-    // I declare and intantiate the objects c and s
+    // I declare and intantiate the objects c and s.
     Circulo c = new Circulo((float)15.2, color);
     Cuadrado s = new Cuadrado((float)2.3, color);
     
     //Print areas
     System.out.println("Area del circulo: "+c.getArea());
     System.out.println("Area del cuadrado: "+s.getArea());
+
+    //Print perimeter, try it.
     
-    // "toString use to print the color 
+    // "toString use to print the color.
    System.out.println(c);
    System.out.println(s);
     
